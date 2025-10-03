7a40e4e0b3ab1e538ceca64e201118efc6259dc5
DandingB <daniel@Daniels-MBP-2.home>
2025-10-03 10:14:22 +0200

    Added Educado colors
    Applied the colors to primary and secondary buttons

--- web/src/shared/components/shadcn/button.tsx
+++ web/src/shared/components/shadcn/button.tsx
@@ -8,13 +8,10 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        primary: "bg-primary-surface-default text-greyscale-text-negative hover:bg-primary-surface-lighter hover:text-primary",
+        secondary: "border-2 border-primary-border-default bg-greyscale-surface-subtle text-secondary-foreground hover:border-primary-border-lighter",
+        destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        outline: "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
@@ -45,7 +42,7 @@ const buttonVariants = cva(
       },
     },
     defaultVariants: {
-      variant: "default",
+      variant: "primary",
       size: "default",
     },
   }
