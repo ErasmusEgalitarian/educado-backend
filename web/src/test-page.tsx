201f68b014e06ec1a1ce03b592c360149a11e651
Smidtty <148750415+Smidtty@users.noreply.github.com>
2025-10-06 09:03:08 +0200

    form- textarea and dropdown
    reusable texarea and dropdown in form

--- web/src/test-page.tsx
+++ web/src/test-page.tsx
@@ -1,4 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { UserIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
@@ -6,14 +7,23 @@ import { z } from "zod";
 import { Form } from "@/shared/components/shadcn/form";
 
 import FormActions from "./shared/components/form/form-actions";
+import { FormDropdown } from "./shared/components/form/form-dropdown";
 import { FormInput } from "./shared/components/form/form-input";
 import { FormPasswordInput } from "./shared/components/form/form-password-input";
+import { FormTextarea } from "./shared/components/form/form-textarea";
 import { Button } from "./shared/components/shadcn/button";
+import { is } from "node_modules/cypress/types/bluebird";
 
 // The zod schema defines both validation and the form's data shape.
 const formSchema = z.object({
   username: z.string().min(2, "Username must be at least 2 characters."),
   password: z.string().min(6, "Password must be at least 6 characters."),
+  message: z.string().optional(),
+  role: z
+    .string({
+      required_error: "You must select a role.",
+    })
+    .min(1, "You must select a role."),
 });
 
 const TestPage = () => {
@@ -52,12 +62,14 @@ const TestPage = () => {
             fieldName="username"
             placeholder="johndoe"
             label="Username"
+            isRequired={true}
           />
           <FormPasswordInput
             control={form.control}
             fieldName="password"
             placeholder="••••••••"
             label="Password"
+            isRequired={true}
             labelAction={
               <Button
                 variant="blank"
@@ -70,6 +82,25 @@ const TestPage = () => {
                 Forgot password?
               </Button>
             }
+          />
+          <FormTextarea
+            control={form.control}
+            fieldName="message"
+            label="Message"
+            placeholder="Write your message here..."
+            rows={4}
+          />
+          <FormDropdown
+            control={form.control}
+            fieldName="role"
+            label="Role"
+            placeholder="Select your role"
+            options={[
+              { label: "Admin", value: "admin" },
+              { label: "User", value: "user" },
+            ]}
+            startIcon={<UserIcon className="mr-2" />}
+            isRequired={true}
           />
           <FormActions
             formState={form.formState}
