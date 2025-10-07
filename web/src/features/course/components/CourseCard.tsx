75a5a5ab18a5ef54719d1456471ed5e1e409eb93
mewkel <mikkelbrunl@gmail.com>
2025-10-07 07:38:27 +0200

    styling of courseCard based on figma

--- web/src/features/course/components/CourseCard.tsx
+++ web/src/features/course/components/CourseCard.tsx
@@ -24,71 +24,71 @@ import { Course } from "../types/Course";
 export const CourseCard = ({ course }: { course: Course }) => {
   const maxTitleLength = 20;
   const maxDescLength = 40;
+
   return (
-    <Card className="w-full max-w-sm flex flex-col justify-between h-full">
-      <div>
-        <CardHeader>
-          <div className="flex items-center gap-2">
-            <svg width="20" height="18" viewBox="0 0 20 18" fill="none">
-              <path
-                d="M20 18H0V0H2V16H4V14H8V16H10V13H14V16H16V14H20V18ZM16 11H20V13H16V11ZM10 3H14V6H10V3ZM14 12H10V7H14V12ZM4 7H8V9H4V7ZM8 13H4V10H8V13Z"
-                fill="#383838"
-              />
-            </svg>
-            <CardTitle className="truncate">
-              {course.title.slice(0, maxTitleLength) +
-                (course.title.length > maxTitleLength ? "..." : "")}
-            </CardTitle>
-          </div>
-
-          <CardDescription className="flex flex-col gap-1">
-            <Badge
-              variant="outline"
-              className="flex items-center gap-1 text-left max-w-40 truncate"
-            >
-              <Icon
-                className="w-5 shrink-0"
-                path={
-                  categories[course.category]?.icon ?? categories.default.icon
-                }
-              />
-              <span className="truncate">
-                {categories[course.category]?.br ?? course.category}
-              </span>
-            </Badge>
-            <p>
-              {course.estimatedHours != null ? course.estimatedHours : "?"}{" "}
-              horas
-            </p>
-          </CardDescription>
-        </CardHeader>
-
-        <CardContent className="mb-2">
-          {course.description.slice(0, maxDescLength) +
-            (course.description.length > maxDescLength ? "..." : "")}
-        </CardContent>
-      </div>
-
-      <CardFooter className="flex flex-col gap-2 items-center mt-auto">
-        <div className="flex gap-2">
-          <Button
-            effect="hoverUnderline"
-            variant="link"
-            onClick={() =>
-              (window.location.href = `/courses/manager/${course._id}/0`)
-            }
+    <Card>
+      <CardHeader className="w-full max-w-sm flex flex-col justify-between h-full">
+        <div className="flex items-center gap-2">
+          <svg width="20" height="18" viewBox="0 0 20 18" fill="none">
+            <path
+              d="M20 18H0V0H2V16H4V14H8V16H10V13H14V16H16V14H20V18ZM16 11H20V13H16V11ZM10 3H14V6H10V3ZM14 12H10V7H14V12ZM4 7H8V9H4V7ZM8 13H4V10H8V13Z"
+              fill="#383838"
+            />
+          </svg>
+          <CardTitle>
+            {course.title.slice(0, maxTitleLength) +
+              (course.title.length > maxTitleLength ? "..." : "")}
+          </CardTitle>
+        </div>
+        <div className="w-9/10 border-t border-[#C1CFD7] my-2 mx-auto"></div>
+      </CardHeader>
+      <CardContent>
+        <div className="flex items-center justify-between">
+          {/* <Badge
+            variant="outline"
+            className="flex items-center gap-1 text-left max-w-40 truncate"
           >
-            Editar
-          </Button>
-          <Button effect="hoverUnderline" variant="link">
-            Visualizar
-          </Button>
+            <Icon
+              className="w-5 shrink-0"
+              path={
+                categories[course.category]?.icon ?? categories.default.icon
+              }
+            />
+            <span className="truncate">
+              {categories[course.category]?.br ?? course.category}
+            </span>
+          </Badge> */}
+
+          <Icon
+            className="w-5 shrink-0 text-[#628397]"
+            path={categories[course.category]?.icon ?? categories.default.icon}
+          />
+          <span class="w-40 text-[#628397] truncate">
+            {categories[course.category]?.br ?? course.category}
+          </span>
+
+          <p className="text-sm text-gray-600">
+            {course.estimatedHours != null ? course.estimatedHours : "?"} horas
+          </p>
         </div>
+
         <div className="flex items-center">
-          <div className="w-32">
-            <StarRating rating={course.rating ?? 0} />
+          <div className="w-25 text-[#F1CC4F]">
+            <StarRating rating={course.rating} />
           </div>
         </div>
+      </CardContent>
+      <CardFooter className="flex justify-end items-center">
+        <Button
+          variant="link"
+          onClick={() =>
+            (window.location.href = `/courses/manager/${course._id}/0`)
+          }
+          className="text-[#246670] decoration-[#246670] underline"
+        >
+          Editar
+        </Button>
+        <Button variant="secondary">Visualizar</Button>
       </CardFooter>
     </Card>
   );
