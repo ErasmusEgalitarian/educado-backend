503be21f016bb578a6828006f3e829f54638896e
CelestrialDragon <gustav.tue@gmail.com>
2025-10-19 12:05:16 +0200

    login system
    Login system no cryptation

--- strapi/src/api/content-creator/controllers/content-creator.ts
+++ strapi/src/api/content-creator/controllers/content-creator.ts
@@ -3,5 +3,53 @@
  */
 
 import { factories } from '@strapi/strapi'
+import  jwt  from "jsonwebtoken";
 
-export default factories.createCoreController('api::content-creator.content-creator');
+export default factories.createCoreController('api::content-creator.content-creator', ({ strapi }) => ({
+  async login(ctx) {
+    try {
+      // Access request data via ctx.request.body
+      const { email, password } = ctx.request.body;
+
+      const user = await strapi.db.query('plugin::users-permissions.user').findOne({
+        where: { email },
+      });
+
+      if (!user) {
+        return ctx.badRequest('Invalid email or password');
+      }
+
+      const isValidPassword = password.compare(user.password); // implement password validation logic here
+    
+
+      if (!isValidPassword) {
+        return ctx.badRequest('Invalid email or password');
+      }
+
+      if (!user.confirmed) {
+        return ctx.badRequest('Admin approval is required.');
+      }
+
+
+      
+    // 3. Generate token
+    const token = jwt.sign(
+      { id: user.id },
+      process.env.JWT_SECRET,
+      { expiresIn: '1d' }
+    );
+
+    // 4. Respond with token and user info
+    ctx.send({
+      jwt: token,
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+      },
+    });
+    } catch (err) {
+      ctx.body = err;
+    }
+  },
+}));
