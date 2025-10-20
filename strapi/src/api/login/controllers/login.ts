4cc4e6a25020d3f3b3ac833ce8c57937b6328017
Ed <KJ36UL@student.aau.dk>
2025-10-20 09:12:48 +0200

    Refactor login controller for clarity and JWT generation
    Simplified and clarified the login controller logic: improved comments, removed unnecessary logs, and ensured password comparison and JWT token generation are handled cleanly. Also updated the documentation generation date.

--- strapi/src/api/login/controllers/login.ts
+++ strapi/src/api/login/controllers/login.ts
@@ -1,14 +1,12 @@
-/**
- * A set of functions called "actions" for `login`
+/* finding the user by provided email, comparing the provided password with the stored hashed password using bcrypt,
+ * and finally generating a JWT token upon successful authentication.
  */
-
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
 export default {
   loginAction: async (ctx, next) => {
     try {
-
       //gets secret key from .env
       const secretKey = process.env.JWT_SECRET;
 
@@ -16,34 +14,31 @@ export default {
       const { email, password } = ctx.request.body;
 
       if (!email || !password) {
-        return ctx.badRequest('Email and password are required');
+        return ctx.badRequest("Email and password are required");
       }
-      // Fetch user from custom 'student' collection type
-      // api is the API namespace, student is the collection type
-      // plugin if you are using a plugin, e.g., users-permissions given by default see in jwtService below
-      const user = await strapi.db.query('api::student.student').findOne({
+      // extracting the user from db by the provided email
+      const user = await strapi.db.query("api::student.student").findOne({
         where: { email },
       });
 
       if (!user) {
-        return ctx.unauthorized('Can not find email');
+        return ctx.unauthorized("Can not find email");
       }
-
-
-      console.log(password);
-      console.log(user.password);
-      //HERE
-      const validPassword = await bcrypt.compare(password.trim(), user.password);
+      // Password comparision using bcrypt
+      const validPassword = await bcrypt.compare(
+        password.trim(),
+        user.password
+      );
       if (!validPassword) {
         return ctx.unauthorized("Invalid password");
       }
 
-      // Generate JWT token and assign to user 
+      // Generate JWT token and assign to user
       let signedUser = jwt.sign(user, secretKey);
 
       ctx.response.body = JSON.stringify(signedUser);
     } catch (err) {
       ctx.body = err;
     }
-  }
+  },
 };
