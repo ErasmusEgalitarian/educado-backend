49c9b8bd2fd3fcbc4b23716ca1b4fdff7c6990f8
Jesper Hjorth <jesper@plaiten.dk>
2025-10-20 14:11:02 +0200

    Working policies, with try catch block for jwt.verify

--- strapi/src/policies/is-contentCreator.ts
+++ strapi/src/policies/is-contentCreator.ts
@@ -2,15 +2,18 @@
  * Policy: is-content-creator
  * 
  * Returns true if the authenticated user exists in the Content Creator collection.
- * 
- * TODO:
- * Implement JWT for decrypting the context token into usable object
  */
 
+import jwt from "jsonwebtoken"
+
 export default async (policyContext, config, { strapi }) => {
+
+    //gets secret key from .env
+    const secretKey = process.env.JWT_SECRET;
+
     // Extract the authenticated user from the policy context
     // This object is populated by Strapi when the user is logged in
-    const user = policyContext.state.user;
+    const user : any = jwt.verify(policyContext.request.ctx.headers.authorization, secretKey);
 
     // If there’s no authenticated user, deny access immediately
     if (!user) {
@@ -21,16 +24,23 @@ export default async (policyContext, config, { strapi }) => {
     try {
         // Query the Content Creator collection to find a record
         // that matches both the user's email and documentId
-        const student = await strapi.db.query('api::content-creator.content-creator').findFirst({
+        const contentCreator = await strapi.documents('api::content-creator.content-creator').findFirst({
             filters: { 
                 email: user.email, 
-                documentId: user.documentId 
+                documentId: user.documentId
             },
         });
 
+        if(user.verifiedAt != null){
+            console.log("Content creator not verified")
+            return false;
+        }
+
+        console.log("is-contentCreator policy passed");
+
         // Return true if a matching Content Creator exists (grant access),
         // or false if not (deny access)
-        return !!student;
+        return !!contentCreator;
     } catch (error) {
         // If an error occurs during the query, log it and deny access
         strapi.log.error('Error in is-content-creator policy:', error);
--- strapi/src/policies/is-student.ts
+++ strapi/src/policies/is-student.ts
@@ -2,15 +2,25 @@
  * Policy: is-student-test policy
  * 
  * Returns true if the authenticated user exists in the Student collection.
- *  
- * TODO:
- * Implement JWT for decrypting the context token into usable object
  */
 
+import jwt from "jsonwebtoken"
+
 export default async (policyContext, config, { strapi }) => {
-    // Extract the authenticated user from the policy context
-    // This object is populated by Strapi when the user is logged in
-    const user = policyContext.state.user;
+    
+    // Gets secret key from .env
+    const secretKey = process.env.JWT_SECRET;
+    let user : any;
+
+    try {
+        // Extract the authenticated user from the policy context
+        // This object is populated by Strapi when the user is logged in
+        const user : any = jwt.verify(policyContext.request.ctx.headers.authorization, secretKey);
+    } catch (error) {
+        strapi.log.error("JWT verification failed:", error);
+        return false;
+    }
+
 
     // If there’s no authenticated user, deny access immediately
     if (!user) {
@@ -21,12 +31,17 @@ export default async (policyContext, config, { strapi }) => {
     try {
         // Query the Student collection to find a record
         // that matches both the user's email and documentId
-        const student = await strapi.db.query('api::student.student').findFirst({
+        const student = await strapi.documents('api::student.student').findFirst({
             filters: { 
                 email: user.email, 
                 documentId: user.documentId 
             },
         });
+
+        if(!user.isVerified){
+            console.log("Student not verified")
+            return false;
+        }
 
         // Return true if a matching Student exists (grant access),
         // or false if not (deny access)
