d6495fe1a1f44a3bc691b831ae9eea67b80de626
Johny1511 <83210690+Johny1511@users.noreply.github.com>
2025-11-03 15:57:56 +0100

    Fix structure for getCourse function and getCertificates. Added strapi fields for date and relation rework

--- strapi/src/api/course-statistics/controllers/course-statistics.ts
+++ strapi/src/api/course-statistics/controllers/course-statistics.ts
@@ -2,35 +2,86 @@
 /**
  * A set of functions called "actions" for `course-statistics`
  */
-
-import jwt, { JwtPayload } from "jsonwebtoken";
 import { errorCodes } from "../../../helpers/errorCodes";
+import jwt, { JwtPayload } from "jsonwebtoken"
 
 export default {
   statisticsAction: async (ctx, next) => {
     try {
       const secretKey = process.env.JWT_SECRET;
-      const user = jwt.verify(ctx.headers.authorization, secretKey);
-      console.log(user);
-      ctx.body = "ok";
+
+
+      /* MOCK JWT INSTEAD OF GETTING IT FROM HEADER */
+        const CC = await strapi.documents('api::content-creator.content-creator').findFirst(
+          {
+            filters: {
+              firstName: "Mama"
+            }
+          }
+        );
+        const CC_type : ContentCreator = {
+          documentId: CC.documentId,
+          name: CC.firstName,
+          email: CC.email,
+          verifiedAt: new Date(CC.verifiedAt)
+        }
+        const jwtCC = jwt.sign(CC_type, secretKey);
+
+
+
+      // Extract the authenticated user from the policy context
+      // This object is populated by Strapi when the user is logged in
+      const user_type = jwt.verify(jwtCC, secretKey) as ContentCreator;
+
+    
     } catch (err) {
+      ctx.status = 500;
       ctx.body = err;
     }
   },
-  creatorCertificatesCount: async (ctx) => {
-    const creatorId = ctx.params.id;
+};
 
-    try {
+
+async function getStudetStats(documentId : string){
+    // Find Content Creator
+  const user = await strapi.documents('api::content-creator.content-creator').findFirst(
+    {
+      populate: {
+        courses: {
+          populate: ["course_relations"]
+        }
+      },
+      filters: {
+        documentId: documentId
+      }
+    }
+  );
+  if (!user) {
+    throw { error: errorCodes['E0504'] }
+  }
+  console.log(user);
+
+  let count = 0;
+  for ( const course of user.courses ) {
+    for ( const courseRelation of course.course_relations){
+      count++;
+      //checkDate
+    }
+  }
+}
+
+async function getCertificates(documentId : string) {
+
+  try {
       const creator = await strapi
         .documents("api::content-creator.content-creator")
         .findOne({
-          documentId: creatorId,
+          documentId: documentId,
           populate: ["courses"],
         });
 
       if (!creator) {
-        ctx.status = 404;
-        ctx.body = { error: errorCodes["E0013"] };
+        throw { error: errorCodes["E0013"] };
         return;
       }
 
@@ -41,10 +92,9 @@ export default {
           filters: { course: { documentId: { $in: courseIds } } },
         });
 
-      ctx.body = { creatorId, totalCertificates };
-    } catch (err) {
-      ctx.status = 500;
-      ctx.body = err;
-    }
-  },
-};
+      return { documentId, totalCertificates };
+  }catch (err) {
+    throw err;
+  }
+}
+
