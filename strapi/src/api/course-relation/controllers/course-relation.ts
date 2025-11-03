ebb9cfb0657471eeb59bc0aff3d945a3daa060b8
Johny1511 <83210690+Johny1511@users.noreply.github.com>
2025-11-03 15:57:56 +0100

    Fix structure for getCourse function and getCertificates. Added strapi fields for date and relation rework

---
+++ strapi/src/api/course-relation/controllers/course-relation.ts
@@ -0,0 +1,7 @@
+/**
+ * course-relation controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::course-relation.course-relation');
---
+++ strapi/src/api/course-relation/routes/course-relation.ts
@@ -0,0 +1,7 @@
+/**
+ * course-relation router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::course-relation.course-relation');
