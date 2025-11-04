92036be9773c0ecd553b69e2048de9a152c0987b
CubeKR1 <106008133+CubeKR1@users.noreply.github.com>
2025-11-04 15:42:40 +0100

    Fixed certificate and test

--- strapi/src/api/course-statistics/controllers/course-statistics.ts
+++ strapi/src/api/course-statistics/controllers/course-statistics.ts
@@ -4,7 +4,6 @@
  */
 import { errorCodes } from "../../../helpers/errorCodes";
 import jwt, { JwtPayload } from "jsonwebtoken";
-const NOW = Date.now(); // Must be here, outside of default export and functions
 const DAYS_30_MS = 30 * 24 * 60 * 60 * 1000;
 const DAYS_7_MS = 7 * 24 * 60 * 60 * 1000;
 
@@ -136,21 +135,15 @@ export async function getCertificatesStats(documentId: string) {
     let count7 = 0;
     let count30 = 0;
 
-    const date30DaysAgo = NOW - DAYS_30_MS;
-    const date7DaysAgo = NOW - DAYS_7_MS;
+    const date30DaysAgo = new Date(Date.now() - DAYS_30_MS);
+    const date7DaysAgo = new Date(Date.now() - DAYS_7_MS);
     // Timestamp for the first day of the current month (e.g., Nov 1, 2025, 00:00:00)
     const firstDayOfMonth = new Date(
       new Date().getFullYear(),
-      new Date().getMonth(),
-      1
-    ).getTime();
+      new Date().getMonth());
 
     for (const cert of certificates) {
-      const certificateWithDate = cert as unknown as {
-        completionDate: string | Date;
-      };
-
-      const issueTime = new Date(certificateWithDate.completionDate).getTime();
+      const issueTime = new Date(cert.completionDate);
 
       // 30 days
       if (issueTime > date30DaysAgo) {
@@ -161,16 +154,18 @@ export async function getCertificatesStats(documentId: string) {
         count7++;
       }
       // This Month
-      if (issueTime >= firstDayOfMonth) {
+      if (issueTime > firstDayOfMonth) {
         countMonth++;
       }
     }
 
     return {
-      totalCertificatesIssued: countTotal,
-      thisMonth: countMonth,
-      last7Days: count7,
-      last30Days: count30,
+      total: countTotal,
+      progress: {
+        lastThirtyDays: Math.round((count30/(countTotal-count30))*100), 
+        lastSevenDays: Math.round((count7/(countTotal-count7))*100), 
+        thisMonth: Math.round((countMonth/(countTotal-countMonth))*100)
+      }
     };
   } catch (err) {
     throw err;
