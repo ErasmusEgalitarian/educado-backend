c4e1284b53c2059cd0097c3ea3cd85c7f5703cef
ollioddi <oliver@roedscholer.dk>
2025-11-11 11:36:36 +0100

    Removed unused table

--- web/src/shared/Table/Table.tsx
+++
@@ -1,6 +0,0 @@
-import { PropsWithChildren } from "react";
-interface TableInterface {}
-
-export const Table = ({ children }: PropsWithChildren<TableInterface>) => {
-  return <table className="table table-fixed w-full">{children}</table>;
-};
--- web/src/shared/Table/TableHead.tsx
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren } from "react";
-interface TableHeadInterface {}
-
-export const TableHead = ({
-  children,
-}: PropsWithChildren<TableHeadInterface>) => {
-  return <thead>{children}</thead>;
-};
