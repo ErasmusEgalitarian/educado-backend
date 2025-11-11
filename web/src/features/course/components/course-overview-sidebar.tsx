c9018a1f78887b8d9656ce07d903303def1804f1
Jesper Hjorth <jesper@plaiten.dk>
2025-11-11 14:29:01 +0100

    Removed unused interface

--- web/src/features/course/components/course-overview-sidebar.tsx
+++ web/src/features/course/components/course-overview-sidebar.tsx
@@ -34,10 +34,6 @@ interface StatisticsResponseBody {
   evaluation: EntityData;
 }
 
-interface StatisticsResponse {
-  responseBody: StatisticsResponseBody;
-}
-
 const OverviewSidebar = ({ documentIds }: { documentIds?: string[] }) => {
   const { t, i18n } = useTranslation();
   const userInfo: userInfo = getUserInfo();
