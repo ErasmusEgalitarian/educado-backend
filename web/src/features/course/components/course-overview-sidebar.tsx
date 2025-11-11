03766827220718645f554a5006ac63a5bc42618a
Jesper Hjorth <jesper@plaiten.dk>
2025-11-11 14:29:01 +0100

    Removed unused interface

--- web/src/features/course/components/course-overview-sidebar.tsx
+++ web/src/features/course/components/course-overview-sidebar.tsx
@@ -32,10 +32,6 @@ interface StatisticsResponseBody {
   evaluation: EntityData;
 }
 
-interface StatisticsResponse {
-  responseBody: StatisticsResponseBody;
-}
-
 const OverviewSidebar = ({ documentIds }: { documentIds?: string[] }) => {
   const { t, i18n } = useTranslation();
   const userInfo: userInfo = getUserInfo();
