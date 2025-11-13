58d49f39f35aa594f6e2141d4af62e65b83009ea
Emil Lykke Grann <emillykkeg@gmail.com>
2025-11-13 10:33:08 +0100

    remove relation

--- strapi/types/generated/contentTypes.d.ts
+++ strapi/types/generated/contentTypes.d.ts
@@ -989,10 +989,6 @@ export interface ApiStudentStudent extends Struct.CollectionTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    user: Schema.Attribute.Relation<
-      'oneToOne',
-      'plugin::users-permissions.user'
-    >;
     user_logs: Schema.Attribute.Relation<'oneToMany', 'api::user-log.user-log'>;
     verifiedAt: Schema.Attribute.Date;
   };
@@ -1553,7 +1549,6 @@ export interface PluginUsersPermissionsUser
       'manyToOne',
       'plugin::users-permissions.role'
     >;
-    student: Schema.Attribute.Relation<'oneToOne', 'api::student.student'>;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
