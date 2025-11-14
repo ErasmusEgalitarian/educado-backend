3b0de8e2830b58c4f41b65d23422f502f136fa53
Emil Lykke Grann <emillykkeg@gmail.com>
2025-11-14 10:16:25 +0100

    Login.tsx working

--- web/src/features/auth/components/password-recovery/NavigationFooter.tsx
+++ web/src/features/auth/components/password-recovery/NavigationFooter.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 
-import { ToggleModalContext } from "../login";
+import { ToggleModalContext } from "../Login";
 
 import { HandleContinueContext } from "./PasswordRecoveryModal";
 
