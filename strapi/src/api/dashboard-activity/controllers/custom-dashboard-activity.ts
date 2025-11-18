9c2d80aff27ac9bd2548853956a7f85429a1cd4d
Johny1511 <83210690+Johny1511@users.noreply.github.com>
2025-11-18 13:01:03 +0100

    dashboard activities with custom get route

---
+++ strapi/src/api/dashboard-activity/controllers/custom-dashboard-activity.ts
@@ -0,0 +1,42 @@
+import { errorCodes } from "../../../helpers/errorCodes";
+import jwt from "jsonwebtoken";
+
+interface Dashboard_activity {
+  description: string;
+  date: Date;
+}
+
+export default {
+  dashboardUserAction: async (ctx, next) => {
+    try{
+      // Extract the authenticated user from the policy context
+      const secretKey = process.env.JWT_SECRET;
+      const user = jwt.verify(ctx.headers.authorization.split("Bearer ")[1], secretKey) as ContentCreator;
+      if(!user){
+        throw "User is undefined";
+      }
+      //Finds the content creator on the backend
+      const contentCreator = await strapi.documents('api::content-creator.content-creator').findFirst(
+        {
+          filters: {
+            email: user.email as string
+          },
+          populate: ['dashboard_activities']
+        }
+      );
+
+      // Pushes all of the Dashboard activites to an array and returns said array.
+      let dashboard_activities : Dashboard_activity[] = [];
+      for (const dashboard_activity of contentCreator.dashboard_activities){
+        dashboard_activities.push({description: dashboard_activity.activityDesc, date: new Date(dashboard_activity.updatedAt)});
+      }
+
+      ctx.response.body = dashboard_activities;
+      
+    } catch (err) {
+        strapi.log.error("Dashboard activity GET failed:", err);
+        ctx.response.status = 500;
+        ctx.response.body = { error: errorCodes["E0019"] };
+    }
+  }
+}
---
+++ strapi/src/api/dashboard-activity/routes/dashboard-activity.ts
@@ -0,0 +1,7 @@
+/**
+ * dashboard-activity router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::dashboard-activity.dashboard-activity');
