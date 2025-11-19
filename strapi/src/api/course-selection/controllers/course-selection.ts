11738064417fe00734747b3251f442327958cba0
PhilipShaffer <152175307+PhilipShaffer@users.noreply.github.com>
2025-11-19 13:33:40 +0100

    Wrote in the controller so that it can populate

--- strapi/src/api/course-selection/controllers/course-selection.ts
+++ strapi/src/api/course-selection/controllers/course-selection.ts
@@ -2,6 +2,36 @@
  * course-selection controller
  */
 
-import { factories } from '@strapi/strapi'
+// src/api/course-selection/controllers/course-selection.ts
+import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::course-selection.course-selection');
+export default factories.createCoreController(
+  'api::course-selection.course-selection',
+  ({ strapi }) => ({
+    async find(ctx) {
+      const result = await strapi
+        .documents('api::course-selection.course-selection')
+        .findMany({
+          ...ctx.query,
+          // here is where you use the populate parameter
+          populate: ['exercises', 'lectures', 'course'], // or "*" or object syntax
+        });
+
+      return this.transformResponse(result);
+    },
+
+    async findOne(ctx) {
+      const { id } = ctx.params;
+
+      const result = await strapi
+        .documents('api::course-selection.course-selection')
+        .findOne({
+          documentId: id,
+          ...ctx.query,
+          populate: ['exercises', 'lectures', 'course'],
+        });
+
+      return this.transformResponse(result);
+    },
+  })
+);
