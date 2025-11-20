84c4ec97299e944c96b821a9837e8d810095deaa
Johny1511 <83210690+Johny1511@users.noreply.github.com>
2025-11-20 13:30:16 +0100

    renamed (course_relation) to (course_enrollment_relation)

--- strapi/src/api/course-relation/routes/course-relation.ts
+++
@@ -1,7 +0,0 @@
-/**
- * course-relation router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::course-relation.course-relation');
--- strapi/tests/api/course-statistics/controllers.test.ts
+++ strapi/tests/api/course-statistics/controllers.test.ts
@@ -10,17 +10,17 @@ describe('Test statistics', () => {
     const mockFilteredCourses = [
         {
             documentId: "course1", createdAt: new Date("2025-10-18"),
-            course_relations: [{enrollmentDate: new Date("2025-11-13")}, {enrollmentDate: new Date("2025-11-14")}, {enrollmentDate: new Date("2025-10-18")}, {enrollmentDate: new Date("2025-11-2")}],
+            course_enrollment_relations: [{enrollmentDate: new Date("2025-11-13")}, {enrollmentDate: new Date("2025-11-14")}, {enrollmentDate: new Date("2025-10-18")}, {enrollmentDate: new Date("2025-11-2")}],
             feedbacks: [{createdAt: new Date("2025-10-20"), rating: 5}, {createdAt: new Date("2025-11-15"), rating: 3}, {createdAt: new Date("2025-11-15"), rating: 3}, {createdAt: new Date("2025-11-15"), rating: 3}],
         },
         {
             documentId: "course2", createdAt: new Date("2025-9-2"),
-            course_relations: [{enrollmentDate: new Date("2025-9-3")}, {enrollmentDate: new Date("2025-9-3")}, {enrollmentDate: new Date("2025-10-18")}, {enrollmentDate: new Date("2025-11-2")}],
+            course_enrollment_relations: [{enrollmentDate: new Date("2025-9-3")}, {enrollmentDate: new Date("2025-9-3")}, {enrollmentDate: new Date("2025-10-18")}, {enrollmentDate: new Date("2025-11-2")}],
             feedbacks: [{createdAt: new Date("2025-10-20"), rating: 5}, {createdAt: new Date("2025-9-4"), rating: 5}, {createdAt: new Date("2025-11-15"), rating: 4}, {createdAt: new Date("2025-11-15"), rating: 4}],
         },
         {
             documentId: "course3", createdAt: new Date("2025-9-2"),
-            course_relations: [{enrollmentDate: new Date("2025-9-3")}, {enrollmentDate: new Date("2025-9-3")}, {enrollmentDate: new Date("2025-10-18")}, {enrollmentDate: new Date("2025-11-2")}],
+            course_enrollment_relations: [{enrollmentDate: new Date("2025-9-3")}, {enrollmentDate: new Date("2025-9-3")}, {enrollmentDate: new Date("2025-10-18")}, {enrollmentDate: new Date("2025-11-2")}],
             feedbacks: [{createdAt: new Date("2025-10-20"), rating: 3}, {createdAt: new Date("2025-9-4"), rating: 2}, {createdAt: new Date("2025-11-15"), rating: 3}, {createdAt: new Date("2025-11-15"), rating: 3}],
         }
     ]
