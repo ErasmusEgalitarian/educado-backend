841b0abe200b27efd1ec77bf77bdca6c8b45902f
EmilLykke <48921722+EmilLykke@users.noreply.github.com>
2025-11-25 14:35:54 +0100

    Override controllers to allow populate (#121)
    * Wrote in the controller so that it can populate
    
    * update inorder to populate so we can find subscribed courses
    
    * Auto enable policies
    
    * Removing manual populate
    
    * Update documentation
    
    * Updated to return pagination on the find
    
    * Updated bootstrapi function
    
    ---------
    
    Co-authored-by: PhilipShaffer <152175307+PhilipShaffer@users.noreply.github.com>
    Co-authored-by: Jakob Rossander Kristensen <jakobrk@gmail.com>
    Co-authored-by: Jakob Rossander Kristensen <152175219+Jakobrk@users.noreply.github.com>

--- strapi/src/api/course-category/controllers/course-category.ts
+++ strapi/src/api/course-category/controllers/course-category.ts
@@ -2,6 +2,32 @@
  * course-category controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::course-category.course-category');
+export default factories.createCoreController(
+  'api::course-category.course-category',
+  ({ strapi }) => ({
+    async find(ctx) {
+      const { results, pagination } = await strapi
+        .service('api::course-category.course-category')
+        .find({
+          ...ctx.query,
+        });
+
+      return this.transformResponse(results, { pagination });
+    },
+
+    async findOne(ctx) {
+      const { id } = ctx.params;
+
+      const result = await strapi
+        .documents('api::course-category.course-category')
+        .findOne({
+          documentId: id,
+          ...ctx.query,
+        });
+
+      return this.transformResponse(result);
+    },
+  })
+);
