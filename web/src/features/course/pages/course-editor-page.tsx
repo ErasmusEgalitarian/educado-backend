bfcb176cbde005adb8a457c2570bef5f9b690a61
Oliver Roed Schøler <oliver@roedscholer.dk>
2025-12-08 10:39:35 +0100

    Added fields and changed mutations

--- web/src/features/course/pages/course-editor-page.tsx
+++ web/src/features/course/pages/course-editor-page.tsx
@@ -124,7 +124,7 @@ const CourseEditorPage = () => {
           documentId: docId,
           title: values.title,
           difficulty: Number(values.difficulty),
-          course_categories: values.categories,
+          course_categories: values.course_categories,
           description: values.description,
           image: imageId,
         });
