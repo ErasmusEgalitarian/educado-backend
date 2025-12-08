f3bb8c25c4966ae043b88977c0c7515e3873b09c
Emil Lykke Grann <emillykkeg@gmail.com>
2025-12-08 14:58:59 +0100

    updated strapi version and updated enrollment endpoint to auth false

--- strapi/src/api/course-enrollment-relation/controllers/course-enrollment-relation.ts
+++ strapi/src/api/course-enrollment-relation/controllers/course-enrollment-relation.ts
@@ -4,4 +4,24 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::course-enrollment-relation.course-enrollment-relation');
+export default factories.createCoreController('api::course-enrollment-relation.course-enrollment-relation', {
+    async find(ctx) {
+        const { results, pagination } = await strapi
+            .service('api::course-enrollment-relation.course-enrollment-relation')
+            .find({
+                ...ctx.query,
+            });
+        return this.transformResponse(results, { pagination });
+    },
+    async findOne(ctx) {
+        const { id } = ctx.params;
+        const result = await strapi
+            .documents('api::course-enrollment-relation.course-enrollment-relation')
+            .findOne({
+                documentId: id,
+                ...ctx.query,
+            });
+        return this.transformResponse(result);
+    },
+}
+);
