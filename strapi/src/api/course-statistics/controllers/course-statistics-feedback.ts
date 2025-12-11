1ed246ff1f11a61dfd41b5b2e45224b75ae7e297
Jesper Hjorth <jesper@plaiten.dk>
2025-12-11 11:56:41 +0100

    Moved courseId check up to before it is used

--- strapi/src/api/course-statistics/controllers/course-statistics-feedback.ts
+++ strapi/src/api/course-statistics/controllers/course-statistics-feedback.ts
@@ -5,33 +5,34 @@ interface Feedback {
 }
 
 export default {
-    async getAverageCourseFeedback(ctx) {
-      try {
-            const courseId = ctx.params.courseId;
-            const course = await strapi.documents("api::course.course").findOne({
-              documentId: courseId,
-              populate: ["feedbacks"],
-            });
-    
-            if (!courseId) {
-              ctx.response.status = 500;
-              ctx.response.body = { error: errorCodes["E0006"] };
-            }
-    
-            const feedbacksArray = (course.feedbacks ?? []) as Feedback[];
-    
-            const averageCourseRating =
-              feedbacksArray.length > 0
-                ? feedbacksArray.reduce((accumulator, currentValue) => accumulator + (currentValue.rating ?? 0), 0) / feedbacksArray.length
-                : 0;
-    
-            return {
-              total: Number(averageCourseRating.toFixed(1)),
-            };
-    
-          } catch (err) {
-          ctx.response.status = 500;
-          ctx.body = err;
-          }
-        }
+  async getAverageCourseFeedback(ctx) {
+    try {
+      const courseId = ctx.params.courseId;
+
+      if (!courseId) {
+        ctx.response.status = 500;
+        ctx.response.body = { error: errorCodes["E0006"] };
+      }
+
+      const course = await strapi.documents("api::course.course").findOne({
+        documentId: courseId,
+        populate: ["feedbacks"],
+      });
+
+      const feedbacksArray = (course.feedbacks ?? []) as Feedback[];
+
+      const averageCourseRating =
+        feedbacksArray.length > 0
+          ? feedbacksArray.reduce((accumulator, currentValue) => accumulator + (currentValue.rating ?? 0), 0) / feedbacksArray.length
+          : 0;
+
+      return {
+        total: Number(averageCourseRating.toFixed(1)),
+      };
+
+    } catch (err) {
+      ctx.response.status = 500;
+      ctx.body = err;
+    }
+  }
 }
