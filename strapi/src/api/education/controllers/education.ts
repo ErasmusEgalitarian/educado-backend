9e1312716125fe2cf96b8c16e42fd4260a476357
Jasper Seneberg <bananfi2k@gmail.com>
2025-12-11 17:16:35 +0100

    add auth policies and custom find methods to education and job routes

--- strapi/src/api/education/controllers/education.ts
+++ strapi/src/api/education/controllers/education.ts
@@ -4,4 +4,30 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::education.education');
+export default factories.createCoreController(
+  'api::education.education',
+  ({ strapi }) => ({
+    async find(ctx) {
+      const { results, pagination } = await strapi
+        .service('api::education.education')
+        .find({
+          ...ctx.query,
+        });
+
+      return this.transformResponse(results, { pagination });
+    },
+
+    async findOne(ctx) {
+      const { id } = ctx.params;
+
+      const result = await strapi
+        .documents('api::education.education')
+        .findOne({
+          documentId: id,
+          ...ctx.query,
+        });
+
+      return this.transformResponse(result);
+    },
+  })
+);
--- strapi/src/api/job/controllers/job.ts
+++ strapi/src/api/job/controllers/job.ts
@@ -4,4 +4,30 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::job.job');
+export default factories.createCoreController(
+  'api::job.job',
+  ({ strapi }) => ({
+    async find(ctx) {
+      const { results, pagination } = await strapi
+        .service('api::job.job')
+        .find({
+          ...ctx.query,
+        });
+
+      return this.transformResponse(results, { pagination });
+    },
+
+    async findOne(ctx) {
+      const { id } = ctx.params;
+
+      const result = await strapi
+        .documents('api::job.job')
+        .findOne({
+          documentId: id,
+          ...ctx.query,
+        });
+
+      return this.transformResponse(result);
+    },
+  })
+);
